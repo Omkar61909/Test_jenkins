08a896f83958745bbdd28ef6dc2ba4358038f904
Omkar More <omkar.more@go-upwards.com>
2020-04-22 18:03:22 +0530

    initial commit

---
+++ request_header_parser/v1/views.py
@@ -0,0 +1,14 @@
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+#from request_header_parser.v1 import serializers as app_serializer
+
+
+class RequestHeaderParserDataDetail(APIView):
+
+	def _ip_address(self, request):
+		pass
+
+
+	def get(self, request, *args, **kwargs):
+		return Response({'hello': 'world'}, status=status.HTTP_200_OK)
