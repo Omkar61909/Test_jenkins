02d053cb0f398bfc3eb089d132784ec451959319
Omkar More <omkar.more@go-upwards.com>
2020-04-22 18:36:16 +0530

    added venv

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/omkar/Desktop/Projects/mini_projects/Test_jekins/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
